78b97d460874d9d9eaaa8a27a2fd8fb4a3ce1b49
Elliton-Luis <ellitonjunior05@gmail.com>
2025-10-26 09:36:55 -0300

    feat: create icon model

---
+++ app/Models/Icon.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Icon extends Model
+{
+    protected $table = "icons";
+
+    protected $fillable = [
+        'name',
+        'class',
+        'type'
+    ];
+}
