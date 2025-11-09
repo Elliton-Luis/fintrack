bac87c08bacc41d7fac09cc8d038a385402599bd
Elliton-Luis <ellitonjunior05@gmail.com>
2025-11-09 18:11:05 -0300

    feat: create hide balance function

--- resources/js/components/TotalBalance.vue
+++ resources/js/components/TotalBalance.vue
@@ -1,14 +1,21 @@
 <template>
   <div class="w-full bg-slate-800 rounded-lg shadow-lg p-6">
-    <h3 class="text-xl font-semibold text-white mb-2">Saldo total</h3>
-    <p class="text-3xl font-bold text-white">
-      {{ formattedBalance }}
+    <div class="flex items-center justify-between mb-2">
+      <h3 class="text-xl font-semibold text-white">Saldo total</h3>
+      <button @click="toggleVisibility" class="text-gray-400 hover:text-white p-2">
+        <i class="pi" :class="isBalanceVisible ? 'pi-eye' : 'pi-eye-slash'"></i>
+      </button>
+    </div>
+
+    <p class="text-3xl font-bold text-white h-[40px]">
+      <span v-if="isBalanceVisible">{{ formattedBalance }}</span>
+      <span v-else>R$ ••••••</span>
     </p>
   </div>
 </template>
 
 <script setup>
-import { computed } from 'vue';
+import { computed, ref } from 'vue';
 
 const props = defineProps({
   balance: {
@@ -17,10 +24,16 @@ const props = defineProps({
   }
 });
 
+const isBalanceVisible = ref(true);
+
 const formattedBalance = computed(() => {
   return props.balance.toLocaleString('pt-BR', {
     style: 'currency',
     currency: 'BRL',
   });
 });
+
+const toggleVisibility = () => {
+  isBalanceVisible.value = !isBalanceVisible.value;
+};
 </script>
