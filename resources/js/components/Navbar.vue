8949e63dc53729d53b84f2b29378c3fd827acd0d
Elliton-Luis <ellitonjunior05@gmail.com>
2025-11-09 18:01:09 -0300

    feat: create navbar and sidebar to mobile and computer

---
+++ resources/js/components/Navbar.vue
@@ -0,0 +1,16 @@
+<template>
+  <nav
+    class="lg:hidden fixed top-0 left-0 right-0 z-20
+           flex items-center justify-between p-4 h-16
+           bg-slate-950 border-b border-slate-800"
+  >
+    <h1 class="text-xl font-bold text-white">FinTrack</h1>
+    <button @click="$emit('toggleMenu')" class="p-2 text-gray-300 hover:text-white">
+      <i class="pi pi-bars text-xl"></i>
+    </button>
+  </nav>
+</template>
+
+<script setup>
+defineEmits(['toggleMenu']);
+</script>
