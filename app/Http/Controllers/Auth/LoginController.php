3ecf72da8e93273b92202742de39d522bca5beb7
Elliton-Luis <ellitonjunior05@gmail.com>
2025-11-09 12:20:57 -0300

    refact: improve the login logic

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -7,5 +7,13 @@ use Illuminate\Http\Request;
 
 class LoginController extends Controller
 {
-    //
+    public function authUser(Request $request){
+
+        $data = $request->only('email','password');
+
+        if(Auth::attempt($data)){
+            return redirect()->route('home.index')->with('success','Usuário autênticado com sucesso');
+        }
+        return redirect()->back()->with('error','Usuário ou Senha incorretos');
+    }
 }
