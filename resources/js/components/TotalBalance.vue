0cadc5fcb1d9bdbf7d8916c4101c9ad99871841a
Elliton-Luis <ellitonjunior05@gmail.com>
2025-11-09 18:01:39 -0300

    feat: create total balance card

---
+++ resources/js/components/TotalBalance.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="w-full bg-slate-800 rounded-lg shadow-lg p-6">
+    <h3 class="text-xl font-semibold text-white mb-2">Saldo total</h3>
+    <p class="text-3xl font-bold text-white">
+      {{ formattedBalance }}
+    </p>
+  </div>
+</template>
+
+<script setup>
+import { computed } from 'vue';
+
+const props = defineProps({
+  balance: {
+    type: Number,
+    default: 0,
+  }
+});
+
+const formattedBalance = computed(() => {
+  return props.balance.toLocaleString('pt-BR', {
+    style: 'currency',
+    currency: 'BRL',
+  });
+});
+</script>
