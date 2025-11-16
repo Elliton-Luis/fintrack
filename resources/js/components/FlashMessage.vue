105e9d0a2c659b407d42b8a993046dc5083012bf
Elliton-Luis <ellitonjunior05@gmail.com>
2025-11-16 12:16:30 -0300

    feat: create general flash message

---
+++ resources/js/components/FlashMessage.vue
@@ -0,0 +1,17 @@
+<template>
+  <div
+    v-if="message"
+    class="fixed top-4 left-1/2 -translate-x-1/2
+           bg-green-600 text-white px-4 py-2 rounded-lg shadow-lg z-[9999]">
+    {{ message }}
+  </div>
+</template>
+
+<script setup>
+defineProps({
+  message: {
+    type: String,
+    default: null
+  }
+})
+</script>
