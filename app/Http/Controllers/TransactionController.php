05bcdaedc6946099b772c50fda565b284caac71d
Elliton-Luis <ellitonjunior05@gmail.com>
2025-11-16 21:00:46 -0300

    feat: send variables to the view, render the dashboard and the transaction view

--- app/Http/Controllers/TransactionController.php
+++ app/Http/Controllers/TransactionController.php
@@ -4,26 +4,50 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use Inertia\Inertia;
+
 use App\Models\Transaction;
+use App\Models\Category;
 
 class TransactionController extends Controller
 {
-    public function store(Request $request){
+
+    public function dashboard()
+    {
+        $categories = Category::select('id', 'name')->get();
+        $recentTransactions = Transaction::latest()->take(5)->with('category.icon')->get();
+        $transactions = Transaction::with('category')->get();
+
+        return Inertia::render('Dashboard', [
+            'categories' => $categories,
+            'transactions' => $transactions,
+            'recentTransactions' => $recentTransactions,
+            'flash' => session('success')
+        ]);
+    }
+
+    public function transactions(){
+        return Inertia::render('Transaction');
+    }
+
+    public function store(Request $request)
+    {
         $validated = $request->validate([
-        'name'        => 'required|string|max:255',
-        'description' => 'nullable|string',
-        'type'        => 'required|in:gain,expense',
-        'value'       => 'required|numeric|min:0',
-        'isRecurring' => 'required|boolean',
+            'title'        => 'required|string|max:255',
+            'description' => 'nullable|string',
+            'type'        => 'required|in:income,expense',
+            'amount'       => 'required|numeric|min:0',
+            'isRecurring' => 'required|boolean',
+            'category_id' => 'required|exists:categories,id',
+            'transaction_date' => 'nullable|date'
         ]);
 
         Transaction::create([
             ...$validated,
-            'category'=> 1,
-            'user_id'=>1
+            'user_id' => 1,
         ]);
 
-        return redirect()->back()->with('success');
-
+        return redirect()->route('dashboard')->with('success','Transição Realizada com Sucesso');
     }
+
 }
