5b5e64e0634d7be63ff57b4669fbf829b6df9a18
Elliton-Luis <ellitonjunior05@gmail.com>
2025-11-23 13:54:42 -0300

    feat: add middleware to all routes

--- routes/web.php
+++ routes/web.php
@@ -4,10 +4,22 @@ use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
 use App\Http\Controllers\TransactionController;
+use App\Http\Controllers\Auth\LoginController;
 
-Route::get('/dashboard', [TransactionController::class, 'dashboard'])->name('dashboard');
+Route::middleware('guest')->group(function () {
 
-Route::get('/transactions', [TransactionController::class, 'transactions'])->name('transactions.index');
-Route::post('/transactions', [TransactionController::class, 'store'])->name('transactions.store');
+    Route::inertia('/', 'Login')->name('login');
+    Route::post('/auth', [LoginController::class, 'authUser'])->name('login.auth');
 
-Route::post('/auth', [LoginController::class, 'authUser'])->name('login.auth');
+});
+
+Route::middleware('auth')->group(function () {
+
+    Route::get('/transactions', [TransactionController::class, 'transactions'])->name('transactions.index');
+    Route::get('/dashboard', [TransactionController::class, 'dashboard'])->name('dashboard');
+    Route::post('/transactions', [TransactionController::class, 'store'])->name('transactions.store');
+
+    Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
+
+});
+
